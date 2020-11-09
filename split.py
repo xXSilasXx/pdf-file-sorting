b1270b38e90a8077c39c6b6cc2399b9529e9d120
xXSilasXx <68000588+xXSilasXx@users.noreply.github.com>
2020-11-09 01:23:15 +0100

    Create split.py
    This specific sub - program takes all the pages in the document and distinguishes two following pages belonging to each other though keeping the difference between first and last page and then sends it to another directory. By changing the for loop you can change the order by which pages are separated.

---
+++ split.py
@@ -0,0 +1,35 @@
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+
+pdf_file_path1 = 'Your file path' # take the document
+
+file_base_name1 = pdf_file_path1.replace('.pdf','') 
+
+output_folder_path = os.path.join(os.getcwd(), 'Final Output') # specify where the splitted files are transferred to be concetenated
+
+pdf1 = PdfFileReader(pdf_file_path1) # create PdfFileReader object
+
+
+counter = 2
+
+sec_count = 1
+
+for page_num in range(pdf1.numPages): # for all the pages in the document
+    pdfWriter1 = PdfFileWriter()
+    pdfWriter1.addPage(pdf1.getPage(page_num))
+
+    if counter% 2 == 0:
+        with open(os.path.join(output_folder_path, 'a{1}.pdf'.format(file_base_name1, sec_count)), 'wb')as f: #take the first page of the n-th contract
+            pdfWriter1.write(f)
+            f.close()
+
+    else:
+        with open(os.path.join(output_folder_path, 'b{1}.pdf'.format(file_base_name1, sec_count)), 'wb')as f: #take the second page of the n-th contract
+            pdfWriter1.write(f)
+            f.close()
+            sec_count += 1
+
+
+
+    counter -= 1
